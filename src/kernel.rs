46fd94d50ef0910cedfcddd88918123e6d5207a2
YdrMaster <ydrml@hotmail.com>
2024-11-07 21:34:51 +0800

    feat: 支持启动 kernel
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,5 +1,12 @@
-﻿use crate::{AsRaw, SvmByte};
-use cl3::kernel::{cl_kernel, release_kernel, set_kernel_arg, set_kernel_arg_svm_pointer};
+﻿use crate::{
+    node::{destruct, NodeParts},
+    AsRaw, CommandQueue, EventNode, SvmByte,
+};
+use cl3::{
+    ext::clEnqueueNDRangeKernel,
+    gl::CL_SUCCESS,
+    kernel::{cl_kernel, release_kernel, set_kernel_arg, set_kernel_arg_svm_pointer},
+};
 use half::{bf16, f16};
 
 #[repr(transparent)]
@@ -19,8 +26,54 @@ impl Drop for Kernel {
     }
 }
 
+impl Kernel {
+    pub fn set_arg(&mut self, index: usize, value: impl Argument) -> &mut Self {
+        value.set_to(self, index);
+        self
+    }
+
+    pub fn launch(
+        &self,
+        global_work_offset: &[usize],
+        global_work_size: &[usize],
+        local_work_size: &[usize],
+        queue: &CommandQueue,
+        node: Option<&mut EventNode>,
+    ) {
+        let work_dim = local_work_size.len();
+        assert_eq!(work_dim, global_work_offset.len());
+        assert_eq!(work_dim, global_work_size.len());
+
+        let NodeParts {
+            num_events_in_wait_list,
+            event_wait_list,
+            event,
+            ..
+        } = destruct(node);
+        assert_eq!(CL_SUCCESS, unsafe {
+            clEnqueueNDRangeKernel(
+                queue.as_raw(),
+                self.0,
+                work_dim as _,
+                global_work_offset.as_ptr(),
+                global_work_size.as_ptr(),
+                local_work_size.as_ptr(),
+                num_events_in_wait_list,
+                event_wait_list,
+                event,
+            )
+        })
+    }
+}
+
 pub trait Argument {
-    fn set(&self, kernel: &mut Kernel, index: usize);
+    fn set_to(&self, kernel: &mut Kernel, index: usize);
+}
+
+impl<T: Argument> Argument for &T {
+    fn set_to(&self, kernel: &mut Kernel, index: usize) {
+        T::set_to(*self, kernel, index)
+    }
 }
 
 macro_rules! impl_for_num {
@@ -28,7 +81,7 @@ macro_rules! impl_for_num {
         $(
             impl Argument for $ty {
                 #[inline]
-                fn set(&self, kernel: &mut Kernel, index: usize) {
+                fn set_to(&self, kernel: &mut Kernel, index: usize) {
                     unsafe {
                         set_kernel_arg(
                             kernel.0,
@@ -55,7 +108,7 @@ impl_for_num! {
 
 impl Argument for *const SvmByte {
     #[inline]
-    fn set(&self, kernel: &mut Kernel, index: usize) {
+    fn set_to(&self, kernel: &mut Kernel, index: usize) {
         unsafe { set_kernel_arg_svm_pointer(kernel.0, index as _, self.cast()) }.unwrap()
     }
 }
